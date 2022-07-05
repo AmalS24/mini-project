a497c81c41c5ca427a2ace64a640b75360c462f3
Amal <78146873+AmalS24@users.noreply.github.com>
2022-07-05 20:34:56 +0530

    Add files via upload

--- admin.py
+++ admin.py
@@ -13,7 +13,7 @@ root = Tk()
 ctypes.windll.shcore.SetProcessDpiAwareness(1)
 root.eval('tk::PlaceWindow . center')
 root.title("LMS_admin_v1.0")
-root.geometry("900x480")
+root.geometry("1200x720")
 root.config(bg="#FFFFFF")
 root.resizable(False,False)
 font = Font(family = "Helvetica", size = 13)
@@ -53,11 +53,14 @@ def render_table(cur):
         dataframe = df,
         showtoolbar=False, 
         showstatusbar=True,
-        width=780,
+        width=1100,
+        height=500,
         editable=False,
         )
     table.show()
-
+    for i in range(4):
+        table.zoomIn()
+    table.expandColumns(factor=30)
 
 #connect to mongodb
 client = MongoClient("mongodb://127.0.0.1:27017/")
@@ -70,27 +73,26 @@ handle_reset()
 dropdown = ["Branch","Date","Type","Name"]
 eq = Label(root)
 eq.config(text="=",font="Arial 12")
-eq.place(x=230,y=420)
+eq.place(x=230,y=640)
 
 ftr_box = ttk.Combobox(root, values=dropdown,state='readonly',style='W.TCombobox',font="Helvetica 13")
-ftr_box.place(x=20,y=420)
+ftr_box.place(x=20,y=640)
 
 value_box = ttk.Entry(root, width=18, font='Arial 12')
-value_box.place(x=250,y=420)
+value_box.place(x=250,y=640)
 
 #buttons
 ftr_btn = ttk.Button(
     root,
     style= 'W.TButton',
     text="Filter",
-    command=handle_filter).place(x=430,y=420)
+    command=handle_filter).place(x=430,y=640)
 
 rst_btn = ttk.Button(
     root,
     style= 'W.TButton',
     text="Reset",
-    command=handle_reset).place(x=530,y=420)
-
+    command=handle_reset).place(x=530,y=640)
 
 # df.to_excel (r'C:\Users\Amal\Desktop\export_dataframe.xlsx', index = False, header=True)
 root.mainloop()
