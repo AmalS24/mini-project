018cceac5b866ae6426163872f7e831e9fc1a88d
Amal <78146873+AmalS24@users.noreply.github.com>
2022-07-23 22:39:13 +0530

    update with id

--- updateOnShutdown.py
+++ updateOnShutdown.py
@@ -1,13 +1,12 @@
 from datetime import datetime
 from pymongo import MongoClient
-import pymongo
+import bson.objectid as ob
 
 client = MongoClient("mongodb://127.0.0.1:27017/")
 db = client['LMS']
-cur = db.info.find({}).sort('_id',pymongo.DESCENDING).limit(1)
-last_person = list(cur).pop(0)
-_id = last_person['_id']
+f = open("C:\\Users\\Amal\\Desktop\\id.txt","r")
+_id = ob.ObjectId(f.read())
+f.close()
 now = datetime.now()
 exit_t = now.strftime("%I:%M %p") 
-
 db.info.update_one({'_id' : _id},{ '$set' :{'Exit_time': exit_t}})
